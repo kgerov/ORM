4410b83d4aee691c08379ad5a2b11a2baf229fe0
kgerov <kgerov@abv.bg>
2015-03-28 16:03:23 +0200

    XML in .NET

---
+++ 07.XMLInDotNet/XMLIn.Net/XMLIn.Net/Tester.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLIn.Net
+{
+    class Tester
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
